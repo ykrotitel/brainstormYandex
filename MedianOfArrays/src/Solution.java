b295699c0f2a88766b777e47f1fa0b6ee29389d3
19300658 <DAnRasskazov@sberbank.ru>
2023-01-24 23:48:58 +0300

    Median of two arrays - Done Longest Palindromic - Solving...

--- MedianOfArrays/src/Solution.java
+++ MedianOfArrays/src/Solution.java
@@ -3,30 +3,82 @@ public class Solution {
         int[] a = new int[2];
         int[] b = new int[2];
 
-        a[0] = 1;
-        a[1] = 2; //3
+        a[0] = 2;
+        a[1] = 4; //3
 
-        b[0] = 3; //2
-        b[1] = 4; //7
+        b[0] = 1; //2
+        b[1] = 3; //7
 
-        double c = findMedianSortedArrays(a, b);
-        System.out.println(c);
+
+        double d = findMedian(a, b);
+        System.out.println(d);
     }
 
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length == 0 && nums2.length == 0)
-            return 0;
-        if (nums1.length == 0 && nums2.length != 0)
-            return findMedian(nums2);
-        else if (nums1.length != 0 && nums2.length == 0)
-            return findMedian(nums1);
-        double a = findMedian(nums1);
-        double b = findMedian(nums2);
-        return (a + b) / 2;
+    public static double findMedian(int[] nums1, int[] nums2) {
+        int[] c = new int[nums1.length + nums2.length];
+
+        int i = 0;
+        while (i != nums1.length) {
+            c[i] = nums1[i];
+            i++;
+        }
+        int j = i;
+        i = 0;
+        while (i != nums2.length) {
+            c[j] = nums2[i];
+            i++;
+            j++;
+        }
+        quickSort(c, 0, c.length - 1);
+
+        double test = findMedian(c);
+        int a = 5;
+        a = a + 1;
+         return findMedian(c);
     }
+
+    public static void quickSort(int []c, int from, int to) {
+        if (from < to) {
+            int pivot = findPivot(c, from, to);
+
+            quickSort(c, from, pivot - 1);
+            quickSort(c, pivot, to);
+        }
+    }
+
+    public static int findPivot(int[] array, int from, int to) {
+        int rIndex = to;
+        int lIndex = from;
+
+        int pivot = array[from + (to - from) / 2];
+        while(lIndex <= rIndex) {
+            while(array[lIndex] < pivot)
+                lIndex++;
+
+            while(array[rIndex] > pivot)
+                rIndex--;
+
+            if (lIndex <= rIndex) {
+                swap(array, lIndex, rIndex);
+                lIndex++;
+                rIndex--;
+            }
+        }
+        return lIndex;
+    }
+
+    public static void swap(int[] array, int left, int right) {
+        int tmp = array[left];
+        array[left] = array[right];
+        array[right] = tmp;
+    }
+
+
+
+
     public static double findMedian(int[] a) {
         if (a.length % 2 == 1)
             return a[a.length / 2];
         return 0.5 * (a[a.length / 2 - 1] + a[a.length / 2]);
     }
-} //TODO для массивов [1, 3] [2, 7] странно находит медиану
+}
--- QuickSort/src/QuickSort.java
+++ QuickSort/src/QuickSort.java
@@ -4,8 +4,11 @@ import java.util.List;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] a = new int[]{10, 14, 13, 21, 56, 32, 1, 5, 6, 7, 4, 3, 2, 1};
+//        int[] a = new int[]{10, 14, 13, 21, 56, 32, 1, 5, 6, 7, 4, 3, 2, 1};
+        int[] a = new int[]{2, 4, 3, 1};
         quickSort(a, 0, a.length - 1);
+
+        System.out.println("this");
     }
 
     public static void quickSort(int[] array, int from, int to) {
@@ -15,6 +18,12 @@ public class QuickSort {
             quickSort(array, from, pivot - 1);
             quickSort(array, pivot, to);
         }
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
     }
 
     public static int divideArray(int[] array, int from, int to) {
@@ -38,9 +47,50 @@ public class QuickSort {
         return lIndex;
     }
 
-    public static void swap(int[] array, int left, int right) {
-        int tmp = array[left];
-        array[left] = array[right];
-        array[right] = tmp;
-    }
+
+
+    //////////////////////
+//    public static void quickSort(int[] arr, int from, int to) {
+//
+//        if (from < to) {
+//
+//            int divideIndex = partition(arr, from, to);
+//
+//            quickSort(arr, from, divideIndex - 1);
+//
+//            quickSort(arr, divideIndex, to);
+//        }
+//    }
+//
+//    private static int partition(int[] arr, int from, int to) {
+//        int rightIndex = to;
+//        int leftIndex = from;
+//
+//        int pivot = arr[from + (to - from) / 2];
+//        while (leftIndex <= rightIndex) {
+//
+//            while (arr[leftIndex] < pivot) {
+//                leftIndex++;
+//            }
+//
+//            while (arr[rightIndex] > pivot) {
+//                rightIndex--;
+//            }
+//
+//            if (leftIndex <= rightIndex) {
+//                swap(arr, rightIndex, leftIndex);
+//                leftIndex++;
+//                rightIndex--;
+//            }
+//        }
+//        return leftIndex;
+//    }
+//
+//    private static void swap(int[] array, int index1, int index2) {
+//        int tmp  = array[index1];
+//        array[index1] = array[index2];
+//        array[index2] = tmp;
+//    }
+    //////////////////////
+
 }
