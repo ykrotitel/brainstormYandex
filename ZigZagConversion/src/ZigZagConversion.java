990717b0dd5eb2366ec6734cfbbd7b22f471ab0e
19300658 <DAnRasskazov@sberbank.ru>
2023-01-26 13:27:04 +0300

    ZigZag - Done !

--- ZigZagConversion/src/ZigZagConversion.java
+++ ZigZagConversion/src/ZigZagConversion.java
@@ -3,30 +3,50 @@ import java.util.List;
 
 public class ZigZagConversion {
     public static void main(String[] args) {
-
+        String s = "A";
+        System.out.println(convert(s, 3));
 
     }
-        public static String convert(String source, int numRows) {
-            List<List <Character>> double_massiv = new ArrayList<>();
-            List<Character> tmp = new ArrayList<>();
 
-            int i = 0;
-            while (i < numRows) {
-                double_massiv.add(tmp);
+    public static String convert(String s, int numRows) {
+        int tmp = (((s.length()/(numRows*2)) - 1 <= 0) ? 1 : ((s.length()/(numRows*2)) - 1) * numRows) + 1;
+        char[][] massiv = new char[numRows][tmp];
+        int pivot = 0;
+        String answer = "";
+        int reference;
+        int i = 0;
+        if (numRows >= s.length())
+            return s;
+        while (i < s.length()) {
+            int j = 0;
+            while (j < numRows && i < s.length()) {
+                massiv[j][pivot] = s.charAt(i);
                 i++;
+                j++;
             }
-
-            i = 0;
-            while (i < source.length()) {
-                int j = 0;
-                while (j < numRows) {
-                    double_massiv.get(j).add(source.charAt(i)); //опустились вниз и записали часть лова
+            j -= 2;
+            reference = j;
+            pivot++;
+            while (j > 0 && i < s.length()) {
+                if (j == reference) {
+                    massiv[j][pivot] = s.charAt(i);
                     i++;
-                    j++;
+                    pivot++;
+                    reference--;
                 }
                 j--;
             }
-            double_massiv.add(tmp);
-
+        }
+        i = 0;
+        while (i < numRows) {
+            int j = 0;
+            while (j < tmp) {
+              if (massiv[i][j] != 0)
+                  answer = answer + massiv[i][j];
+              j++;
+            }
+            i++;
+        }
+        return answer;
     }
 }
